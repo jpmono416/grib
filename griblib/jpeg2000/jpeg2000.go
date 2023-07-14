bf6e68c165be034298e21bf8d7310a8b619ba903
Jonathan.Perez <Jonathan.Perez@campbellsci.co.uk>
2023-07-14 16:32:24 +0100

    Added simple jpeg2000 wrapper lib

---
+++ griblib/jpeg2000/jpeg2000.go
@@ -0,0 +1,53 @@
+// {{{ Copyright (c) Paul R. Tagliamonte <paultag@gmail.com>, 2019
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. }}}
+
+// Using this package will require cgo linking to imagick.
+// On GNU/Linux this means installing the `libmagickwand-dev` package.
+
+// Package jpeg2000 contains a thin wrapper on top of imagick to convert
+// JPEG2000 bytes to a golang image.Image.
+// https://pkg.go.dev/pault.ag/go/cbeff/jpeg2000
+package jpeg2000
+
+import (
+	"bytes"
+	"image"
+	"image/png"
+
+	"gopkg.in/gographics/imagick.v2/imagick"
+)
+
+// Parse a JEPG 2000 into an image.Image.
+func Parse(data []byte) (image.Image, error) {
+	imagick.Initialize()
+	defer imagick.Terminate()
+
+	mw := imagick.NewMagickWand()
+	defer mw.Destroy()
+
+	if err := mw.ReadImageBlob(data); err != nil {
+		return nil, err
+	}
+
+	mw.SetImageFormat("png")
+	return png.Decode(bytes.NewReader(mw.GetImageBlob()))
+}
+
+// vim: foldmethod=marker
